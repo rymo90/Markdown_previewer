c8eb8d9cc6ad83e7da95209433a0a02d2a4854ff
rymo <red01.yassin@gmail.com>
2018-06-12 00:44:59 +0200

    added new components

---
+++ src/components/Editor.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class Editor extends React.Component {
+  render() {
+    console.log(this.props);
+
+    return (
+      <div className="Editor">
+      my editor
+      </div>
+    );
+  }
+}
+
+
+export default Editor;
---
+++ src/components/Reviewer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+class Reviewer extends React.Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div className="Reviewer">
+      my Review
+
+      </div>
+    );
+  }
+}
+export default Reviewer;
