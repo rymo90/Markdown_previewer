90c0c6be97d4f8fd1ec346755cdade2ada996f71
rymo <red01.yassin@gmail.com>
2018-06-24 15:27:33 +0200

    markdonw provier workd !

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
 import React from 'react';
 import './App.css';
-import Reviewer from './components/Reviewer';
-import Editor from './components/Editor';
-class App extends React.Component {
+import Markdown from 'react-markdown'
+
+const name = "Redwan";
+const text = `Hello There Obi Wan konobi!
+  how are you to day
+  i'm fine and you
+  what 's you name my name is ${name}'
+`
+
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.handleChange= this.handleChange.bind(this);
+    this.state = {text: text};
+  }
+  handleChange(event){
+    this.setState({text:event.target.value})
+  }
+
   render() {
     return (
-      <div className="App">
-        <h1>components </h1>
-        <Reviewer />
-        <Editor />
+      <div className ="App" >
+        <div>
+          <header className ="App-header">
+            <h2>Free Code Camp Project</h2>
+            <h3>Front End Libraries Projects-Build a Markdown Previewer</h3>
+          </header>
+        </div>
 
+          <textarea id="Editor" value = {this.state.text} onChange ={this.handleChange} >
+          </textarea>
+          <div id='Previewer' >
+            <Markdown
+              escapeHtml = {true}
+              source = {this.state.text}
+              />
+          </div>
       </div>
     );
   }
 }
-
 
 export default App;
